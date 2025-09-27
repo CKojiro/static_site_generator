8d4e32a92540c8e283843987efddf2ae6007c0ab
CKojiro <smiths12324@gmail.com>
2025-09-26 18:20:26 -0700

    Set up generator to work with Github Pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,7 +3,7 @@ import os
 from split_delimiter import markdown_to_html_node
 from extract_title import extract_title
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath="/"):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     try:
         with open(from_path, "r") as file:
@@ -19,17 +19,19 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(from_path_contents)
     full_html = template_path_contents.replace("{{ Title }}", title, 1)
     full_html = full_html.replace("{{ Content }}", html_string, 1)
+    full_html = full_html.replace('href="/', f'href="{basepath}')
+    full_html = full_html.replace('src="/', f'src="{basepath}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as html_file:
         html_file.write(full_html)
 
-def generate_pages_recursively(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursively(dir_path_content, template_path, dest_dir_path, basepath="/"):
     for entry in os.listdir(dir_path_content):
         entry_path = os.path.join(dir_path_content, entry)
-        dest_path = os.path.join(dest_dir_path, entry)
         if os.path.isdir(entry_path):
-            os.makedirs(dest_path, exist_ok=True)
-            generate_pages_recursively(entry_path, template_path, dest_path)
+            new_dest = os.path.join(dest_dir_path, entry)
+            os.makedirs(new_dest, exist_ok=True)
+            generate_pages_recursively(entry_path, template_path, new_dest, basepath)
         elif entry.endswith(".md"):
             name_without_ext = os.path.splitext(entry)[0]
             if entry == "index.md":
--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
+import sys
+
 from copy_source_to_dir import *
 from generate_page import generate_pages_recursively
 
 def main():
-    copy_source_to_dir("static", "public")
-    generate_pages_recursively("content", "template.html", "public")
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+    copy_source_to_dir("static", "docs")
+    generate_pages_recursively("content", "template.html", "docs", basepath)
 
 if __name__ == "__main__":
     main()
